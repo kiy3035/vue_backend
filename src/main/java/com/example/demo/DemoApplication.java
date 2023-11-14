388c2fb855467878bc49465c91f9710884772cdb
Limsukhyun <as17269@naver.com>
2023-11-14 16:14:22 +0900

    feat: vue 백엔드작업

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,18 +8,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @CrossOrigin(origins = "http://localhost:8080")
-@MapperScan(basePackages = {"com.example.demo.video.mapper", 
-                            "com.example.demo.login.mapper",
-                            "com.example.demo.mypage.mapper"}) // Mapper 인터페이스가 위치한 패키지를 정확하게 지정
+@MapperScan(basePackages = {"com.example.demo.video.mapper", "com.example.demo.login.mapper"}) // Mapper 인터페이스가 위치한 패키지를 정확하게 지정
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**") // 해당 패턴에 대해서 CORS 설정을 추가
+                        .allowedOrigins("http://localhost:8001") // 허용할 오리진(Origin) 설정
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드 설정
+                        .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization") // 허용할 헤더 설정
+                        .allowCredentials(true); // Credentials 허용 여부
+            }
+        };
+    }
+    
     @Bean
     public ErrorViewResolver customErrorViewResolver() {
         final ErrorViewResolver resolver = (request, status, model) -> {
