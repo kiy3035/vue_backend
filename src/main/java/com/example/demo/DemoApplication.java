19a99911b6dd5fbf50d490776d9a49468f6c823f
김인영 <116708377+kiy3035@users.noreply.github.com>
2023-11-14 15:09:37 +0900

    mypage추가

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,9 @@ import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 @CrossOrigin(origins = "http://localhost:8080")
-@MapperScan(basePackages = {"com.example.demo.video.mapper", "com.example.demo.login.mapper"}) // Mapper 인터페이스가 위치한 패키지를 정확하게 지정
+@MapperScan(basePackages = {"com.example.demo.video.mapper", 
+                            "com.example.demo.login.mapper",
+                            "com.example.demo.mypage.mapper"}) // Mapper 인터페이스가 위치한 패키지를 정확하게 지정
 public class DemoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/demo/mypage/dto/MypageDto.java
@@ -0,0 +1,15 @@
+package com.example.demo.mypage.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class MypageDto {
+
+    @Getter @Setter
+    private String email;
+
+    @Getter @Setter
+    private String password;
+
+    
+}
