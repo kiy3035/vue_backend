795f3b902a179546221d0e024e2cd45944d43b7a
김인영 <116708377+kiy3035@users.noreply.github.com>
2023-11-17 10:07:53 +0900

    feat: vue 백엔드작업

--- src/main/java/com/example/demo/video/controller/VideoController.java
+++ src/main/java/com/example/demo/video/controller/VideoController.java
@@ -2,6 +2,7 @@ package com.example.demo.video.controller;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -31,17 +33,24 @@ public class VideoController {
     private VideoService videoService;
 
     @PostMapping("/api/upload")
-    public String uploadVideo(@ModelAttribute VideoDto videoDto, @RequestParam("videoFile") MultipartFile videoFile) {
+    public String uploadVideo(@RequestPart("videoFile") MultipartFile videoFile, VideoDto videoDto, @RequestParam("videoTitle") String title,
+                                                                    @RequestParam("videoContent") String content) {
+        System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+        System.out.println(title);
+        System.out.println(content);
 
         // 파일 이름을 video_no로 설정
         String originalFilename = videoFile.getOriginalFilename();
         String extension = originalFilename.substring(originalFilename.lastIndexOf('.'));
         String videoNo = System.currentTimeMillis() + extension;
+
         // 업로드된 비디오 파일과 DTO를 서비스에 전달
         videoDto.setVideo_no(videoNo);
         videoDto.setFilename(videoNo); // 파일 이름을 videoNo로 설정
-
         videoDto.setVideoFile(videoFile);
+        videoDto.setTitle(title);
+        videoDto.setContent(content);
+
 
         // 데이터베이스에 비디오 정보를 저장
         String result = videoService.uploadVideo(videoDto, videoFile); // 데이터베이스에 비디오 정보를 저장하도록 수정
