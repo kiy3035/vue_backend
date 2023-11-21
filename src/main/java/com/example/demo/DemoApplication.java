b9c079e71b470a7f57557f430b92d6b0b9ca4bb2
김인영 <116708377+kiy3035@users.noreply.github.com>
2023-11-22 08:58:39 +0900

    i go home

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,7 +13,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @CrossOrigin(origins = "http://localhost:8001")
-@MapperScan(basePackages = {"com.example.demo.video.mapper", "com.example.demo.login.mapper", "com.example.demo.mypage.mapper"}) // Mapper 인터페이스가 위치한 패키지를 정확하게 지정
+@MapperScan(basePackages = { "com.example.demo.video.mapper"
+                           , "com.example.demo.login.mapper"
+                           , "com.example.demo.mypage.mapper"
+                        //    , "com.example.demo.like.mapper" 
+                        }) // Mapper 인터페이스가 위치한 패키지를 정확하게 지정
 public class DemoApplication {
 
     public static void main(String[] args) {
@@ -25,7 +29,7 @@ public class DemoApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**") // 해당 패턴에 대해서 CORS 설정을 추가
+                registry.addMapping("/**") // 해당 패턴에 대해서 CORS 설정을 추가
                         .allowedOrigins("http://localhost:8001") // 허용할 오리진(Origin) 설정
                         .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드 설정
                         .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization") // 허용할 헤더 설정
@@ -34,14 +38,14 @@ public class DemoApplication {
         };
     }
     
-    @Bean
-    public ErrorViewResolver customErrorViewResolver() {
-        final ErrorViewResolver resolver = (request, status, model) -> {
-            if (status == HttpStatus.NOT_FOUND) {
-                return new ModelAndView("forward:/test.html");
-            }
-            return null;
-        };
-        return resolver;
-    }
+    // @Bean
+    // public ErrorViewResolver customErrorViewResolver() {
+    //     final ErrorViewResolver resolver = (request, status, model) -> {
+    //         if (status == HttpStatus.NOT_FOUND) {
+    //             return new ModelAndView("forward:/test.html");
+    //         }
+    //         return null;
+    //     };
+    //     return resolver;
+    // }
 }
---
+++ src/main/java/com/example/demo/like/service/LikeService.java
@@ -0,0 +1,12 @@
+// package com.example.demo.like.service;
+
+// import java.util.List;
+// import java.util.Map;
+
+// import com.example.demo.like.dto.LikeDto;
+
+// public interface LikeService {
+//     void updateUserInfo(Map<String, Object> userInfo);
+//     // String login(Map<String, Object> userInfo);
+
+// }
